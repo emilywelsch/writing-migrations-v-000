018c0a7ebebab8be789f6b295edfd0b43bd318e3
Emily Welsch <emwelsch@gmail.com>
2018-07-29 19:48:16 +0000

    automatically backed up by learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,8 +1,7 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
   def change
-    add_column :students do |student|
-      student.integer :grade
-      student.string :birthdate
+    add_column :students, :grade, :integer
+    add_column :students, :birthdate, :string
     end
   end
 end
